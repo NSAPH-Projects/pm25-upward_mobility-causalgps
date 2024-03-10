bcbc7241dac2eb5b506df9bf7650d03b22657669
luca-merlo <132392800+luca-merlo@users.noreply.github.com>
2024-03-10 22:36:21 +0100

    Update entropy_balance_ebal.R

--- code/entropy_balance_ebal.R
+++ code/entropy_balance_ebal.R
@@ -56,3 +56,54 @@ lmi = lmer(formula = lkfr_pooled_pooled_p25 ~ zpm25_1982 + (1 | county_state), d
 lms = lmer(formula = lkfr_pooled_pooled_p25 ~ zpm25_1982 + (1 + zpm25_1982 | county_state), data = atlas_subset, weights = mod$weights_cap) 
 # if you see any warning add the following command:
 # control = lmerControl(optCtrl=list(maxfun=2e5), optimizer = "bobyqa")
+
+# compare the goodness of fit of the two models (random intercepts vs random intercepts and slope)
+anova(lmi, lms)
+nc <- detectCores()
+cl <- makeCluster(rep("localhost", nc))
+pbkrtest::PBmodcomp(largeModel = lms, smallModel = lmi, nsim = 50, cl = cl, seed = 101)
+
+# US Map plot (just an example to see the results)
+dta.map = data.frame(fips = rownames(ranef(lms)$county_state), value = ranef(lms)$county_state[,1])
+plot_usmap(data = dta.map, values = "value") + 
+  scale_fill_continuous(
+    low = "blue", high = "red", name = "Random intercepts", label = scales::comma
+  ) + 
+  theme(legend.title=element_text(size=18), legend.text = element_text(size=15), legend.position = "right")
+
+# US Map plot (random intercepts and slopes quartiles)
+qnle = 4
+county.slope = percent_change((fixef(lms)[2] + est$zpm25_1982)) # 100 * (fixef(lms)[2] + est$zpm25_1982)
+dta.map = data.frame(fips = rownames(ranef(lms)$county_state), value = cut(county.slope, quantile(county.slope, 0:qnle/qnle), dig.lab = 2))
+plot_usmap(data = dta.map, values = "value", exclude = c("AK","HI")) + 
+  scale_fill_manual(values = c(colorRampPalette(c("blue", "red"))(qnle)), name = "", guide = guide_legend(reverse = TRUE), na.translate = F) +
+  theme(legend.title=element_text(size=18), legend.text = element_text(size=30))
+
+# NPML
+K = 1:10 # number of finite mixture components
+tmp.npi = lapply(K, function(k) 
+  allvc(lkfr_pooled_pooled_p25 ~ zpm25_1982, random = ~1|county_state, data = atlas_subset, random.distribution = "np", k = k, weights = mod$weights_cap, lambda = 0.99, plot.opt = 0))
+BIC = sapply(1:length(K), function(k) tmp.npi[[k]]$disparity + 1e250 * any(tmp.npi[[k]]$masses < 0.01)) + log(no_counties) * (1 + K + K - 1 + K)
+ICL = BIC - 2 * sapply(1:length(K), function(k) sum(tmp.npi[[k]]$post.prob * ifelse(tmp.npi[[k]]$post.prob > 0, log(tmp.npi[[k]]$post.prob), 0)))
+
+BIC
+which.min(BIC)
+npi = tmp.npi[[which.min(BIC)]]
+
+tmp.nps = lapply(K, function(k) 
+  allvc(lkfr_pooled_pooled_p25 ~ zpm25_1982, random = ~zpm25_1982|county_state, data = atlas_subset, random.distribution = "np", k = k, weights = mod$weights_cap, lambda = 0.99, plot.opt = 0))
+BIC = sapply(1:length(K), function(k) tmp.nps[[k]]$disparity + 1e250 * any(tmp.nps[[k]]$masses < 0.01)) + log(no_counties) * (K + K + K - 1 + K)
+ICL = BIC - 2 * sapply(1:length(K), function(k) sum(tmp.nps[[k]]$post.prob * ifelse(tmp.nps[[k]]$post.prob > 0, log(tmp.nps[[k]]$post.prob), 0)))
+
+BIC
+which.min(BIC)
+nps = tmp.nps[[which.min(BIC)]] 
+
+# US Map plot (just an example to see the results)
+dta.map = aggregate(apply(nps$post.prob, 1, which.max), by = list(nps$data$county_state), mean)
+colnames(dta.map) = c("fips", "value")
+plot_usmap(data = dta.map, values = "value") + 
+  scale_fill_continuous(
+    low = "blue", high = "red", name = "Class posterior probability", label = scales::comma
+  ) + 
+  theme(legend.title=element_text(size=18), legend.text = element_text(size=15), legend.position = "right")
